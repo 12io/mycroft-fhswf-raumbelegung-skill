7f419212eee749984e48904ade22625c1569a022
Silvio Marra @ BA Picroft device <marra.silvio@fh-swf.de>
2021-05-22 04:50:16 +0200

    room occupancy overview

--- __init__.py
+++ __init__.py
@@ -1,54 +1,75 @@
+import re
 import requests
 import xml.etree.ElementTree as ET
 from datetime import date
+from adapt.intetn import IntentBuilder
+from mycroft import MycroftSkill, intent_handler
 
-def getXML(standort=0, semester=0, tag=0):
-    if not standort:
-        standort = "Is"
-    if not semester:
-        heute = date.today()
-        if 3<= heute.month <=8:
-            semester="SS" + str(heute.year)
-        else:
-            semester="WS" + str(heute.year)
-    if not tag:
-        tag = date.today()
+locationMap = {'Is': 'Iserlohn', 'Ha': 'Hagen', 'Ls': 'Lüdenscheid', 'Me': 'Meschede', 'So': 'Soest', 'Z': 'Hagen IAH'}
+def getVPISActivities(location=None, semester=None, day=None):
+    response=''
+    url=''
 
-    url="https://vpis.fh-swf.de/" + str(semester) + "/raum.php3?Standort=" + str(standort) + "&Tag=" + str(tag) + "&Template=XML"
-    # return requests.get(url).content
-    return(print(url))
+    if not location:
+        raise AttributeError('Missing parameter: location')
 
-#with open ('test.xml', 'wb') as f:
-#    f.write(getXML("","","2021-04-30"))
-
-def parseXML():
-    #tree = ET.parse(getXML(""))
-    raumbelegung = {}
-    tree = ET.parse('test.xml')
-    vpisResponseXML = tree.getroot()
-    for activityXML in vpisResponseXML.findall('./activities/activity'):
-        modulname=activityXML.find('./name').text
-        veranstaltungstyp=activityXML.find('./activity-type').text
-        activityDates=activityXML.findall('./activity-dates/activity-date')
-        raeume=activityXML.findall('./activity-locations/activity-location')
-        for raum in raeume:
-            for activityDate in activityDates:
-                datum=activityDate.get('date')
-                start=activityDate.get('begin')
-                ende=activityDate.get('end')
-                #print("In {} findet am {} das Modul {} von {} bis {} als {} statt.".format(raum.text, datum, modulname, start, ende, veranstaltungstyp))
-                
-                # raumnummer1 => datum1 => uhrzeit1 => veranstaltung1
-                #                          uhrzeit2 => veranstaltung2
-                #                          uhrzeit3 => veranstaltung3
-                #             => datum2 => uhrzeit1 => veranstaltung1
-                #                          uhrzeit2 => veranstaltung2
-                #             => datum3 => uhrzeit1 => veranstaltung1
-                # raumnummer2 => datum1 => uhrzeit1 => veranstaltung1
+    vpisApiXmlResponse = requests.get('https://vpis.fh-swf.de/vpisapp.php')
+        
+    if not vpisApiXmlResponse.status_code == 200:
+        raise RuntimeError('Could not connect to VPIS: HTTP status code ' + str(vpisApiXmlResponse.status_code))
     
-    for raum in raumbelegung:
-        for datum in raum:
-            for uhrzeit in datum:
-                print("{} -> {} -> {} -> {}".format(raum, datum, uhrzeit, uhrzeit[name]))
+    elif not re.search('application/xml', vpisApiXmlResponse.headers['content-type']):
+        raise RuntimeError('Response is no valid XML.')
+        
+    locationsXml = ET.fromstring(vpisApiXmlResponse.content)
+    for locationChild in locationsXml.findall('./locations'):
+        if locationChild.text == location:
+            url=locationChild.get('href')
+            break
+    finalUrl = requests.get(url).url
+    
+    if semester: finalUrl = re.sub('[WS]S[0-9]{4}', semester, finalUrl) 
+    if day: finalUrl += "&Tag=" + str(date.today())
 
-parseXML()
+    return requests.get(finalUrl).content
+
+def parseVpisXml(xml, fromFile=0):
+    vpisResponseXML = ET.parse(xml).getroot() if fromFile == 1 else ET.fromstring(xml)
+    
+    # Die Raumbelegung für den gesamten Standort X
+    raumbelegung = {} # für die Suche nach einem Raum
+
+    # für jedes <activity>-Element innerhalb des <activities>-Element im XML-Pfad
+    # oder auch: für jede veranstaltungs...
+    for activityXML in vpisResponseXML.findall('./activities/activity'):
+        # ... den Namen, die Art, Tagen an denen Sie stattfinden sowie Räume in denen sie stattfinden zwischenspeichern
+        veranstaltungsname=activityXML.find('./name').text
+        veranstaltungsart=activityXML.find('./activity-type').text
+        veranstaltungstage=activityXML.findall('./activity-dates/activity-date')
+        veranstaltungsraeume=activityXML.findall('./activity-locations/activity-location')
+
+        # für jeden Raum 
+        for raum in veranstaltungsraeume:
+            raumnummer = raum.text
+            for tag in veranstaltungstage:
+                veranstaltungsdatum = tag.get('date')
+                veranstaltungsbeginn = tag.get('begin')
+                veranstaltungsende = tag.get('end')
+                veranstaltung = {'name': veranstaltungsname, 'typ': veranstaltungsart, 'ende': veranstaltungsende}
+                if raumnummer not in raumbelegung:
+                    raumbelegung[raumnummer] = {}
+                if veranstaltungsdatum not in raumbelegung[raumnummer]:
+                    raumbelegung[raumnummer][veranstaltungsdatum] = {}
+                if veranstaltungsbeginn not in raumbelegung[raumnummer][veranstaltungsdatum]:
+                    raumbelegung[raumnummer][veranstaltungsdatum][veranstaltungsbeginn] = veranstaltung
+        return raumbelegung
+
+class FhRoomOccupancySkill(MycroftSkill):
+    def __init__(self):
+        super(FhRoomOccupancySkill, self).__init__(name="FhRoomOccupancySkill")
+    
+    def __initialize__(self):
+        self.speak("Hallo, ich bin der FhRoomOccupancySkill, oder auch zu Deutsch: FhRaumbelegungsSkill")
+
+def create_skill():
+    return FhRoomOccupancySkill()
