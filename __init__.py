e7042af0033a8e793199b230a6673a061ac8abdb
Silvio Marra @ BA Picroft device <marra.silvio@fh-swf.de>
2021-05-24 00:07:46 +0200

    skill testing

--- __init__.py
+++ __init__.py
@@ -2,68 +2,69 @@ import re
 import requests
 import xml.etree.ElementTree as ET
 from datetime import date
-from adapt.intent import IntentBuilder
+from multi_key_dict import multi_key_dict
 from mycroft import MycroftSkill, intent_handler
+from os import mkdir
+from os.path import join
 
-locationMap = {'Is': 'Iserlohn', 'Ha': 'Hagen', 'Ls': 'Lüdenscheid', 'Me': 'Meschede', 'So': 'Soest', 'Z': 'Hagen IAH'}
-def getVPISActivities(location=None, semester=None, day=None, file=0):
-    response=''
-    url=''
+VPIS_FALLBACK_URL='https://vpis.fh-swf.de/vpisapp.php'
 
-    if not location:
-        raise AttributeError('Missing parameter: location')
+fhswfLocationMap = multi_key_dict()
+fhswfLocationMap['iserlohn', 'is', 'frauenstuhlweg','frauenstuhl weg', 'campus iserlohn'] = 'Iserlohn'
+fhswfLocationMap['hagen', 'ha', 'haldener strasse', 'haldener straße', 'haldener str', 'campus hagen'] = 'Hagen'
+fhswfLocationMap['lüdenscheid', 'luedenscheid', 'ls', 'bahnhofsallee', 'campus luedenscheid', 'campus lüdenscheid'] = 'Lüdenscheid'
+fhswfLocationMap['meschede', 'me', 'lindenstrasse','lindenstraße','linden straße','linden strasse','linden str', 'campus meschede'] = 'Meschede'
+fhswfLocationMap['soest', 'so', 'lübecker ring', 'luebecker ring', 'campus soest'] = 'Soest'
+# location "Im Alten Holz" seems to be invalid for API
+fhswfLocationMap['im alten holz', 'hagen iah', 'ha iah', 'hagen im alten holz'] = 'Hagen IAH'
 
-    vpisApiXmlResponse = requests.get('https://vpis.fh-swf.de/vpisapp.php')
+fhswfLocationVpisShortKey = {'Iserlohn': 'Is', 'Hagen': 'Ha', 'Lüdenscheid': 'Ls', 'Meschede': 'Me', 'Soest': 'So' } #, 'Hagen IAH': 'Z'}
+
+def getVPISActivities(location, semester = None, day = None, url = VPIS_FALLBACK_URL):
+    """Queries VPIS for ooccupied rooms.
+
+    Parameters
+    ----------
+    location: string
+        Required. Should contain a location which is mapped to a short string within locationMap-Array.
+        VPIS API has no implementation for inter-site queries for multiple locations.
+    
+    semester: string, default = None
+        Optional. VPIS API always uses current semester. But if given we use it here
+        to request the right semester url for data.
+
+    day: string, default = None
+        Optional. VPIS API always uses current day. But if given, we append it to the request url
+        to get the data for a specific day.
+    
+    url: string, default = VPIS_FALLBACK_URL
+        Optional. Can be used to override VPIS API url. Change it in settingsmeta.json of this Skill.
+
+    Returns
+    -------
+    occupiedRooms: { <roomNumber>: { <day>: { <time>: { <activityName>: }{}}}}}
+        Nested dict containing all activites for the queried day with roomNumber as keys to query for specific activities by roomNumber.
         
-    if not vpisApiXmlResponse.status_code == 200:
-        raise RuntimeError('Could not connect to VPIS: HTTP status code ' + str(vpisApiXmlResponse.status_code))
+    courses: { <courseName>: { <day>: { <roomNumber>: {<time>: {}}}}}
+        Nested dict containing all activities for the queried day with courseName as keys to query for specific activity by courseName.
     
-    elif not re.search('application/xml', vpisApiXmlResponse.headers['content-type']):
-        raise RuntimeError('Response is no valid XML.')
-        
-    locationsXml = ET.fromstring(vpisApiXmlResponse.content)
-    for locationChild in locationsXml.findall('./locations'):
-        if locationChild.text == location:
-            url=locationChild.get('href')
-            break
+    file: string
+    Notes
+    -----
+    An example of the returned data structures:
     
-    finalUrl = requests.get(url).url
-    
-    if semester:
-        finalUrl = re.sub('[WS]S[0-9]{4}', semester, finalUrl)
-    
-    if day:
-        finalUrl += '&Tag=' + day
-
-    vpisXml = requests.get(finalUrl).content
-    
-    if not file == 0:
-        with open(file, 'wb') as f:
-            f.write(vpisXml)
-
-    return requests.get(finalUrl).content
-
-def parseVpisXml(xml, file=0):
-    vpisResponseXML = ET.parse(file).getroot() if not file == 0 else ET.fromstring(xml)
-    
-    # Example for occupiedRooms structure:
-    '''
-    occupiedRooms = { 'Is-H409': { '2021-04-12': { '09:45': { 'Programmierung mit C++2': { 'typ': 'Praktikum', 'ende': '11:15' },
-                                                   '08:00': { 'Programmierung mit C++2': { 'typ': 'Praktikum', 'ende': '09:30' },
-                                                   '12:00': { 'Programmierung mit C++2': { 'typ': 'Praktikum', 'ende': '13:30' }
+    occupiedRooms = { 'Is-H409': { '2021-04-12': { '09:45': { 'Programmierung mit C++2': { 'type': 'Praktikum', 'ende': '11:15' },
+                                                   '08:00': { 'Programmierung mit C++2': { 'type': 'Praktikum', 'ende': '09:30' },
+                                                   '12:00': { 'Programmierung mit C++2': { 'type': 'Praktikum', 'ende': '13:30' }
                                    },
-                                   '2021-04-19': { '09:45': { 'Programmierung mit C++2': { 'typ': 'Praktikum', 'ende': '11:15' },
-                                                   '08:00': { 'Programmierung mit C++2': { 'typ': 'Praktikum', 'ende': '09:30' },
-                                                   '12:00': { 'Programmierung mit C++2': { 'typ': 'Praktikum', 'ende': '13:30' }
+                                   '2021-04-19': { '09:45': { 'Programmierung mit C++2': { 'type': 'Praktikum', 'ende': '11:15' },
+                                                   '08:00': { 'Programmierung mit C++2': { 'type': 'Praktikum', 'ende': '09:30' },
+                                                   '12:00': { 'Programmierung mit C++2': { 'type': 'Praktikum', 'ende': '13:30' }
                                    }
                       },
                       'Is-ZE04': { ... }
     }
-    '''
-    occupiedRooms = {}
-
-    # Example for courses structure:
-    '''
+    
     courses = 'Programmierung mit C++2': { '2021-04-12': { 'Is-H409': { '09:45': { 'type': 'Praktikum', 'end': '11:15' },
                                                                         '08:00': { 'type': 'Praktikum', 'end': '09:30' },
                                                                         '12:00': { 'type': 'Praktikum', 'end': '13:30' }
@@ -75,12 +76,53 @@ def parseVpisXml(xml, file=0):
                                                          }
                                            },
               'Projekt (Systemintegration)': { ... }
-    '''
+    }    
+    """
+    
+    if not location in fhswfLocationMap:
+        raise AttributeError('Invalid parameter: location')
+
+    if not url:
+        url = VPIS_FALLBACK_URL
+
+    vpisControlResponse = requests.get(url)
+        
+    if not vpisControlResponse.status_code == 200:
+        raise RuntimeError('Could not connect to VPIS: HTTP status code ' + str(vpisControlResponse.status_code))
+    
+    elif not re.search('application/xml', vpisControlResponse.headers['content-type']):
+        raise RuntimeError('Response is no valid XML.')
+
+    vpisControlXml = vpisControlResponse.content
+    
+    locationsXml = ET.fromstring(vpisControlXml)
+    for locationChild in locationsXml.findall('./locations'):
+        if locationChild.text == fhswfLocationMap[location]:
+            url=locationChild.get('href')
+            break
+    
+    finalUrl = requests.get(url).url
+    
+    if semester:
+        finalUrl = re.sub('[WS]S[0-9]{4}', semester, finalUrl)
+    
+    if day:
+        finalUrl += '&Tag=' + day
+
+    vpisResponse = requests.get(finalUrl)
+
+    if not vpisResponse.status_code == 200:
+        raise RuntimeError('Could not connect to VPIS: HTTP status code ' + str(vpisResponse.status_code))
+    elif not re.search('application/xml', vpisResponse.headers['content-type']):
+        raise RuntimeError('Response is no valid XML.')
+
+    vpisResponseXml = ET.fromstring(vpisResponse.content)
+    occupiedRooms = {}
     courses = {}
 
     # for each <activity> element within <activities> element inside XML
     # "for each course..."
-    for activityXML in vpisResponseXML.findall('./activities/activity'):
+    for activityXML in vpisResponseXml.findall('./activities/activity'):
         # ... save name, type, all dates(!) for this semester, all rooms(!) for this semester
         courseName=activityXML.find('./name').text
         courseType=activityXML.find('./activity-type').text
@@ -116,7 +158,39 @@ def parseVpisXml(xml, file=0):
                 if courseTimeBegin not in courses[courseName][courseDate][roomNumber]:
                     courses[courseName][courseDate][roomNumber][courseTimeBegin] = courseDetails
         
-    return {'occupiedRooms': occupiedRooms, 'courses': courses}
+    return occupiedRooms, courses
+
+def getRoomsByLocations(url = VPIS_FALLBACK_URL):
+    vpisControlResponse = requests.get(url)
+    if not vpisControlResponse.status_code == 200:
+        raise RuntimeError("Connect to " + url + " failed. HTTP response code: " + vpisControlResponse.status_code)
+    elif not re.search('application/xml', vpisControlResponse.headers['content-type']):
+        raise RuntimeError('Response is no valid XML.')
+    
+    controlXml = ET.fromstring(vpisControlResponse.content)
+ 
+    roomNumbersByLocation = {}
+    for locationChild in controlXml.findall('./locations'):
+        # for fhswfLocation in fhswfLocationVpisShortKey.keys():
+        if locationChild.text not in fhswfLocationMap.values():
+            continue
+        vpisLocationResponse = requests.get(locationChild.get('href'))
+        if not vpisLocationResponse.status_code == 200:
+            raise RuntimeError("Could not fetch rooms for {}. HTTP response code: {}".format(locationChild.text, vpisLocationResponse.status_code))
+        elif not re.search('application/xml', vpisLocationResponse.headers['content-type']):
+            raise RuntimeError('Response is no valid XML.')
+        
+        locationsXml = ET.fromstring(vpisLocationResponse.content)
+        for location in locationsXml.findall('./locations/location'):
+            locationName = str(locationChild.text)
+            room = str(location.find('./name').text)
+            roomWithoutLocationPrefix = re.findall(r'(?:[A-Za-z]{2}-)(.*)', room)[0]
+
+            if locationName not in roomNumbersByLocation:
+                roomNumbersByLocation[locationName] = []
+            if roomWithoutLocationPrefix not in roomNumbersByLocation[locationName]:
+                roomNumbersByLocation[locationName].append(roomWithoutLocationPrefix)
+    return roomNumbersByLocation
 
 class FhRoomOccupancySkill(MycroftSkill):
     def __init__(self):
@@ -127,14 +201,20 @@ class FhRoomOccupancySkill(MycroftSkill):
         self.register_entity_file('location.entity')
         self.log.info('FhRoomOccupancySkill initialized.')
 
-    @intent_handler('fhswf-help.intent')
-    def handleFhSwfIntent(self, message):
-        self.speak_dialog('fhswf')
-
-    @intent_handler('what.does.take.place.in.x.intent')
-    def handleRoomOccupation(self, message):
-        self.log.debug(message)
+    # Padatious
+    @intent_handler('tell.me.about.this.skill.intent')
+    def tellMeAbout(self, message):
+        print('print(\'NOW IN TELL ME ABOUT THIS SKILL INTENT\')')
+        print(message)
+        log.debug('NOW IN TELL ME ABOUT THIS SKILL INTENT')
+        log.debug(message)
+        self.speak_dialog('you.can.ask.me.about.rooms.and.courses')
         
+    @intent_handler('how.do.i.query.for.a.room.intent')
+    def handleHowDoIqueryAroom(self, message):
+        self.log.debug(message)
+        self.speak('Du könntest mich zum Beispiel folgendes fragen:')
+        self.speak_dialog('this.is.how.you.query.for.a.room')
 
 def create_skill():
     return FhRoomOccupancySkill()
